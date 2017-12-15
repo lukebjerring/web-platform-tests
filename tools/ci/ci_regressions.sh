48f3f4decd657700b2947a96d22e30670b420051
Luke Bjerring <lukebjerring@chromium.org>
2017-12-15 14:07:54 -0500

    Add Travis build rule for regressions

---
+++ tools/ci/ci_regressions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
+WPT_ROOT=$(readlink -f $SCRIPT_DIR/../..)
+cd $WPT_ROOT
+
+source tools/ci/lib.sh
+
+check_regressions() {
+    python ./tools/ci/check_regressions.py $PRODUCT --output-bytes $((1024 *1024 * 3)) --metadata ~/meta/ --install-fonts
+}
+
+main() {
+    hosts_fixup
+    if [ $(echo $PRODUCT | grep '^chrome:') ]; then
+       install_chrome $(echo $PRODUCT | grep --only-matching '\w\+$')
+    fi
+    check_regressions
+}
+
+main
